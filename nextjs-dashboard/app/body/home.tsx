342306c2467b29ee8021aadf22d69acdfb00d8bf
Crawford Young <crawfordyoung248@gmail.com>
2025-06-28 13:52:02 -0500

    added image not formatted

--- nextjs-dashboard/app/body/home.tsx
+++ nextjs-dashboard/app/body/home.tsx
@@ -1,9 +1,12 @@
 import "./body.css";
+import Image from "next/image";
+import CampHeadshot from "@/app/photos/CampHeadshot.jpg";
 
 export const Home = () => {
   return (
     <div className="homeContainer">
       <h1>Crawford Young</h1>
+      <Image src={CampHeadshot} alt="Me!" />
       <p>SWE</p>
     </div>
   );
