9640d802eec71209fee1f3bc8cd0f80f55a9ccda
Crawford Young <crawfordyoung248@gmail.com>
2025-07-01 09:22:03 -0500

    aligned profile pic

--- nextjs-dashboard/app/body/home.tsx
+++ nextjs-dashboard/app/body/home.tsx
@@ -1,12 +1,12 @@
 import "./body.css";
 import Image from "next/image";
-import CampHeadshot from "@/app/photos/CampHeadshot.jpg";
+import Headshot from "@/app/photos/profilePic.jpg";
 
 export const Home = () => {
   return (
     <div className="homeContainer">
       <h1>Crawford Young</h1>
-      <Image src={CampHeadshot} alt="Me!" />
+      <Image src={Headshot} alt="Me!" className="profile-circle" />
       <p>SWE</p>
     </div>
   );
