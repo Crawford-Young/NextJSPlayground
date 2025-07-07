792306577de807875bb5337d9e70c447e33f0944
Crawford Young <crawfordyoung248@gmail.com>
2025-07-07 09:07:32 -0500

    info

--- nextjs-dashboard/app/body/about.tsx
+++ nextjs-dashboard/app/body/about.tsx
@@ -4,6 +4,11 @@ export const About = () => {
   return (
     <div className="generalContainer">
       <h3 className="title">About Me</h3>
+      <p>SWE Intern @ FloQast</p>
+      <p>CS @ Auburn</p>
+      <p>President of the Auburn Web Development Club</p>
+      <p>Auburn Competitive Programming Team</p>
+      <p>Tribal Director @ Camp Rockmont</p>
     </div>
   );
 };
